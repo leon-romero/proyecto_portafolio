d75577d8567b684f2144ba65d175aa9a2dded288
leon romero <leon.romero@alumnos.duoc.cl>
2020-07-04 19:36:35 -0400

    Update excel.js

--- public/dist/js/excel.js
+++ public/dist/js/excel.js
@@ -1,4 +1,4 @@
-<script type="text/javascript"> 
+
   var tableToExcel = (function() { 
     var uri = 'data:application/vnd.ms-excel;base64,' 
     , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>' 
@@ -11,4 +11,3 @@
       } 
     })
   (); 
-</script> 
